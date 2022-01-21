3c4fb17e9ec4ab4a821b710f44dab5f0fcd62936
Botv3v <98142914+Botv3v@users.noreply.github.com>
2022-01-21 12:34:59 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -24,16 +24,16 @@ else if (Config.WORKTYPE == 'public') {
 
     Julie.addCommand({pattern: 'owner', fromMe: false, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
-        if (message.jid === '54218542512-1612300121@g.us') {
+        if (message.jid === '9072501166-1612300121@g.us') {
 
             return;
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*LIZA-MWOL by Fayas and Nithin*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*LIZA-MWOL by ADHI and Nithin*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.OWNER + '\n\n--------------------', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.OWNER + '\n\n-------------AdHI SER------', MessageType.text);
         }
     }));
 }
